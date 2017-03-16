5f3ddf506692e35cdef6e3c9e353877cfc620d4e
Ben Melosh <benmelosh@gmail.com>
2017-03-15 17:11:54 -0700

    updates to plotting fugacity and strain rate calculator

--- flowstress.py
+++ flowstress.py
@@ -1,131 +1,222 @@
-%matplotlib nbagg
+# This code calculates strain rates, slip rates at a range of different temperatures incorporating changes 
+# in fugacity as a function of temperature.
 
-import matplotlib
-import numpy as n
-import matplotlib.pyplot as plt
-import os
+
+import numpy as np 
 import math
-from matplotlib import cm
-
- #Calculate differential stress
-d = 9.2# grain size in microns
-B = 2451 #2451 (holyoke 2010) #3631 (stipp 2003)
-m = -1.26 # 
-s = []# (MPa) Differential Stress
-
-part = (math.log(d)-math.log(B))/m
-s = math.exp(part)
-s
+from __future__ import division # Import this to divide floats in python 2
+from scipy import optimize as opt 
 
 
-#Define Constants
-R = 8.3144598 #Gas constant
-w = 30000 # (mm) shear zone width
+# Calculate differential stress
+d = 9.2 # measured grain size in microns
 
-#Define flow laws
-FL = []#Citation, A, N, Q (in that order)
-FL = [
-      #['GT95wom', 0.00011, 4, 137000],
-      ['KT84', 0.0000022, 2.7, 120000],
-      ['GT95wm', 0.000000018, 4, 137000],
-      ['J84', 0.00288, 1.8, 151000],
-      ['K89', 0.0000011, 2.7, 134000],
-      ['HC82', 0.0199, 1.8, 167000],
-      ['LP92g', 0.000000066, 3.1, 135000],
-      ['LP92a', 3.98E-10, 4, 135000],
-      ['H01', 6.3E-12, 4, 135000],
-      ['RB04',0.000012, 2.97, 242000]
-      ]
+# Coefficients for calculating differential stress.
+B = 2451 # 2451 (Holyoke 2010) #3631 (Stipp and Tullis, 2003)
+m = -1.26 # Stress exponent, from Stipp and Tullis (2003).
 
-#Set temperature range
-Tc = []
-for i in range(300, 602, 25):#min, max, step interval
-    T = i
-    Tc.append(T)
-Tk = []
-for i in range(len(Tc)): #Convert to Kelvin
-    T = Tc[i]+273.15
-    Tk.append(T)
 
-#Calculate strain rate for all flow laws over temperature range, store in list
-e = []
-for j in range(0,len(FL)):
-    f = []
-    for i in range(0, len(Tk)):
-        element = FL[j][1]*s**(FL[j][2])*math.exp(-FL[j][3]/(R*Tk[i]))#Flow law equation
-        f.append(element)
-        if len(f) == len(Tk):
-            e.append(f)
-            del f
 
-#Include effect of water fugacity
-F = 300 #MPa (Water Fugacity)
-e = []
-for j in range(0,len(FL)):
-    f = []
-    for i in range(0, len(Tk)):
-        element = FL[j][1]*s**(FL[j][2])*F*math.exp(-FL[j][3]/(R*Tk[i]))#Flow law equation
-        f.append(element)
-        if len(f) == len(Tk):
-            e.append(f)
-            del f
 
-#Calculate slip rate
-V = []
-for i in range(0, len(FL)):
-    f = []
-    for j in range(0, len(Tk)):
-        element = w*31540000*e[i][j]#slip rate calculation
-        f.append(element)
-        if len(f) == len(Tk):
-            V.append(f)
-            del f
+#Create dictionary of flow law coefficients from literature.
+"""
+KT84: Kronenberg and Tullis, 1984
+GT95wm: Gleason and Tullis, 1995 with melt
+J84: Jaoul et al., 1984
+K89: Koch, 1989
+HC82: Hansen and Carter, 1982
+LP92g: Luan and Patterson, 1992 with gel
+LP92a: Luan and Patterson, 1992 with acid
+H01: Hirth et al., 2001
+RB04: Rutter and Brodie, 2004
+"""
+flow_laws = {
+    "KT84": {"A": 2.2E-6, "n": 2.7, "Q": 1.2E5},
+    'GT95wm': {"A": 1.8E-8, "n": 4, "Q": 1.37E5},
+    'J84': {"A": 2.88E-3, "n": 1.8, "Q": 1.51E5},
+    'K89': {"A": 1.1E-6, "n": 2.7, "Q": 1.34E5},
+    'HC82': {"A": 1.99E-2, "n": 1.8, "Q": 1.67E5},
+    'LP92g': {"A": 6.6E-8, "n": 3.1, "Q": 1.35E5},
+    'LP92a': {"A": 3.98E-10, "n": 4, "Q": 1.35E5},
+    "H01":  {"A": 6.3E-12, "n": 4, "Q": 1.35E5}, 
+    "RB04": {"A": 1.2E-5, "n": 2.97, "Q": 2.42E5}
+    }
 
-###PLOT Strain Rate
-colors = iter(cm.jet(np.linspace(0, 1, len(e))))#set color ramp to jet with a line spacing of length e
 
-plt.gca().set_yscale('log') #set y axis to log 
- 
-labels = []
-for i in range(0,len(FL)):
-    l = FL[i][0]
-    labels.append(l)
-    plt.plot(Tc, e[i], color=next(colors))
+#Define some basic functions
+
+def temp_conv(T, dir = "C2K"):  #Convert from C to Kelvin, or Kelvin to C with correct argument
+    if arg == "C2K":
+        Tk = T+273.15
+        return Tk
+    elif arg == "K2C":
+        Tc = T-273.15
+        return Tc
+    else:
+    	print('ERROR: Not a valid selection')
+
+def pres_conv(Pmpa): #Convert from MPa to Pa
+    Ppa = round(Pmpa*1.0E6)
+    return Ppa
+
+
+#Defining PT Condition calculators and depth simulator, 
+#alternatively use a range of temperatures at a given depth and pressure
+
+def pressure_calculator(depth, density): #depth in km, density in g/cm3
+    g = 9.8 #gravity
+    h = depth*1000 #converting to SI
+    row = density*1000 #converting to SI
+    pressure = (row*g*h)/1.0E6 # pressure in MPa
+    return pressure 
+
+def temperature_calculator(depth, geothermal_gradient): #km, C/km
+    temp = geothermal_gradient*depth 
+    return temp
     
+def depth_simulator(depth_range, density=2.7, geothermal_gradient=30): #depth range in km
+    depth_conds = []
+    for dep in range(depth_range[0], depth_range[1]):
+        ptc = (round(pressure_calculator(dep, density)), temperature_calculator(geothermal_gradient, dep)) 
+        depth_conds.append(ptc)
+    return depth_conds
 
+
+
+#Calculating strain rate and slip rate, default is the Hirth et al (2001) flow law.
+
+def calculate_strain_rate(s, flow_law = 'H01'): #s is differential stress, defaults to using Hirth et al flow law
+    e = (flow_laws[flow_law]['A']*np.power(s,flow_laws[flow_law]['n'])*np.power(fugacity,1)*np.exp(-flow_laws[flow_law]['Q']/(8.3144598*temperature)))
+    return e
+
+def calculate_slip_rate(e, w): #w: width in m, output of mm/yr
+    width = w*1000
+    v = width*31536000*e
+    return v
+
+
+
+
+#Create the shear zone class
+
+class Shearzone():
+    def __init__(self, grain_size, constant_b, exponent):
+        self.constant_b = constant_b 
+        self.grain_size = grain_size 
+        self.exponent = exponent
     
-plt.xlabel('Temperature ( C)')
-plt.ylabel('Strain Rate (1/s)')
-plt.title('Flow Laws')
-#for i in range(0, len(Tc)):
- #   plt.text(450,e[i][6],labels[i], fontsize=10, rotation = 8)
-plt.legend(labels, fontsize=10, loc=4)
-plt.show()
+    def calculate_differential_stress(self): 
+        part = (math.log(self.grain_size)-math.log(self.constant_b))/self.exponent
+        s = math.exp(part)
+        return s
+
+#Create grain size simulator
+def simulate_grain_size(range_min, range_max):
+    B = 2451 #2451 (holyoke 2010) #3631 (stipp 2003)
+    m = -1.26 # 
+    diffs = []
+    for x in range(range_min,range_max):
+        part = (math.log(x)-math.log(B))/m
+        s = math.exp(part)
+        diffs.append(s)
+    return diffs 
 
 
-###PLOT Slip Rate
-colors = iter(cm.jet(np.linspace(0, 1, len(V))))#set color ramp to jet with a line spacing of length e
-
-plt.gca().set_yscale('log') #set y axis to log 
- 
-labels = []
-for i in range(0,len(FL)):
-    l = FL[i][0]
-    labels.append(l)
-    plt.plot(Tc, V[i], color=next(colors))
-    
-plt.xlabel('Temperature ( C)')
-plt.ylabel('Velocity (mm/yr)')
-plt.title('Slip Rates')
-#for i in range(0, len(Tc)):
- #   plt.text(450,e[i][6],labels[i], fontsize=10, rotation = 8)
-plt.legend(labels, fontsize=10, loc=4)
-plt.show()
 
 
-#Save figure to pdf
-from matplotlib.backends.backend_pdf import PdfPages
+#Calculate fugacities.
 
-with PdfPages('SlipRatesNEW.pdf') as pdf:#Enter file name
-    pdf.savefig()  # saves the current figure into a pdf page
+#This section was adapted from PS.js by Tony Withers, see http://publish.uwo.ca/~awither5/fugacity/index.htm 
+#for online calculator.
+
+# Create a blank array
+PScoeff = np.zeros([10,10])
+
+# Input coefficients into array
+PScoeff[0][2]=0.24657688*math.pow(10,6)
+PScoeff[0][3]=0.51359951*math.pow(10,2) 
+PScoeff[1][2]=0.58638965*math.pow(10,0) 
+PScoeff[1][3]=-0.28646939*math.pow(10,-2) 
+PScoeff[1][4]=0.31375577*math.pow(10,-4) 
+PScoeff[2][2]=-0.62783840*math.pow(10,1) 
+PScoeff[2][3]=0.14791599*math.pow(10,-1) 
+PScoeff[2][4]=0.35779579*math.pow(10,-3) 
+PScoeff[2][5]=0.15432925*math.pow(10,-7) 
+PScoeff[3][3]=-0.42719875*math.pow(10,0) 
+PScoeff[3][4]=-0.16325155*math.pow(10,-4) 
+PScoeff[4][2]=0.56654978*math.pow(10,4) 
+PScoeff[4][3]=-0.16580167*math.pow(10,2) 
+PScoeff[4][4]=0.76560762*math.pow(10,-1) 
+PScoeff[5][3]=0.10917883*math.pow(10,0) 
+PScoeff[6][0]=0.38878656*math.pow(10,13) 
+PScoeff[6][1]=-0.13494878*math.pow(10,9) 
+PScoeff[6][2]=0.30916564*math.pow(10,6)
+PScoeff[6][3]=0.75591105*math.pow(10,1) 
+PScoeff[7][2]=-0.65537898*math.pow(10,5) 
+PScoeff[7][3]=0.18810675*math.pow(10,3) 
+PScoeff[8][0]=-0.14182435*math.pow(10,14) 
+PScoeff[8][1]=0.18165390*math.pow(10,9) 
+PScoeff[8][2]=-0.19769068*math.pow(10,6)
+PScoeff[8][3]=-0.23530318*math.pow(10,2)
+PScoeff[9][2]=0.92093375*math.pow(10,5)
+PScoeff[9][3]=0.12246777*math.pow(10,3)
+
+
+#Create function to make coefficient table for each T condition.
+#Input coefficients in array from Eq 4 of Pitzer and Sterner (1994).
+
+cs = np.zeros([10]) # Create blank array
+def coeff_table():
+    for i in range(0, 10):
+        cs[i]=PScoeff[i][0]*math.pow(temperature,-4)+PScoeff[i][1]*math.pow(temperature,-2)\
+        +PScoeff[i][2]*math.pow(temperature,-1)\
+        +PScoeff[i][3]+PScoeff[i][4]*temperature+PScoeff[i][5]*math.pow(temperature,2)
+        
+#Call this function    
+#coeff_table()
+
+
+#Calculating equation of state and fugacity
+
+#Solve Equation of state, Eq 2 of Pitzer and Sterner (1994)
+#Returns pressure in Pa
+def eos(T, V):
+    den = 1/V
+    R = 8314472
+    var_num = cs[2]+2*cs[3]*den+3*cs[4]*math.pow(den,2)+4*cs[5]*math.pow(den,3)
+    var_denom = math.pow((cs[1]+cs[2]*den+cs[3]*math.pow(den,2)+cs[4]*math.pow(den,3)+cs[5]*math.pow(den,4)),2)
+    pressure=den+cs[0]*math.pow(den,2)-math.pow(den,2)*(var_num/var_denom)
+    pressure= pressure + (cs[6]*math.pow(den,2)*math.exp(-cs[7]*den)+cs[8]*math.pow(den,2)*math.exp(-cs[9]*den))
+    pressure = pressure*(R*T) #pressure in Pa
+    return pressure
+
+#Solve for fugacity, Eq 1 of Pitzer and Sterner (1994)
+#Returns fugacity in MPa
+def PSfug(P,T,V):
+    den=1/V;
+    R=8314472;
+    quotient = cs[0]*den+(1/(cs[1]+cs[2]*den+cs[3]*math.pow(den,2)+cs[4]*math.pow(den,3)+cs[5]*math.pow(den,4))-1/cs[1])
+    quotient-= cs[6]/cs[7]*(math.exp(-cs[7]*den)-1)
+    quotient-= cs[8]/cs[9]*(math.exp(-cs[9]*den)-1)
+    lnf=(math.log(den)+ quotient+P/(den*R*T))+math.log(R*T)-1
+    return math.exp(lnf)/1e6 # fugacity in MPa
+
+
+#Optimizing equation to solve for volume 
+def fun(v):
+    return eos(temperature, v)- pressure
+volume = opt.brentq(fun, 5, 30) #Volume in cc/mol
+
+print(temperature, pressure)
+
+
+# #Calculate fugacity 
+# fugacity = PSfug(pressure, temperature, volume)
+# print(fugacity)
+
+
+
+
+
+
 
