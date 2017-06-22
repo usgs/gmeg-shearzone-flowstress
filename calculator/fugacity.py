b91b7e1490b496b705e9414b67820c8efb7ff4b5
Ben Melosh <bmelosh@usgs.gov>
2017-06-21 17:33:06 -0700

    running tests

--- calculator/fugacity.py
+++ calculator/fugacity.py
@@ -3,7 +3,48 @@ from scipy.constants.constants import C2K
 import math
 from scipy import optimize as opt # for optimization
 
- #Calculating equation of state and fugacity
+
+PS_COEFF = np.zeros([10,10])
+
+PS_COEFF[0][2]=0.24657688*math.pow(10,6)
+PS_COEFF[0][3]=0.51359951*math.pow(10,2) 
+PS_COEFF[1][2]=0.58638965*math.pow(10,0) 
+PS_COEFF[1][3]=-0.28646939*math.pow(10,-2) 
+PS_COEFF[1][4]=0.31375577*math.pow(10,-4) 
+PS_COEFF[2][2]=-0.62783840*math.pow(10,1) 
+PS_COEFF[2][3]=0.14791599*math.pow(10,-1) 
+PS_COEFF[2][4]=0.35779579*math.pow(10,-3) 
+PS_COEFF[2][5]=0.15432925*math.pow(10,-7) 
+PS_COEFF[3][3]=-0.42719875*math.pow(10,0) 
+PS_COEFF[3][4]=-0.16325155*math.pow(10,-4) 
+PS_COEFF[4][2]=0.56654978*math.pow(10,4) 
+PS_COEFF[4][3]=-0.16580167*math.pow(10,2) 
+PS_COEFF[4][4]=0.76560762*math.pow(10,-1) 
+PS_COEFF[5][3]=0.10917883*math.pow(10,0) 
+PS_COEFF[6][0]=0.38878656*math.pow(10,13) 
+PS_COEFF[6][1]=-0.13494878*math.pow(10,9) 
+PS_COEFF[6][2]=0.30916564*math.pow(10,6)
+PS_COEFF[6][3]=0.75591105*math.pow(10,1) 
+PS_COEFF[7][2]=-0.65537898*math.pow(10,5) 
+PS_COEFF[7][3]=0.18810675*math.pow(10,3) 
+PS_COEFF[8][0]=-0.14182435*math.pow(10,14) 
+PS_COEFF[8][1]=0.18165390*math.pow(10,9) 
+PS_COEFF[8][2]=-0.19769068*math.pow(10,6)
+PS_COEFF[8][3]=-0.23530318*math.pow(10,2)
+PS_COEFF[9][2]=0.92093375*math.pow(10,5)
+PS_COEFF[9][3]=0.12246777*math.pow(10,3)
+
+cs = np.zeros([10]) 
+
+def calculate_coefficient_table(temperature):
+    for i in range(0, len(PS_COEFF)):
+        cs[i]=PS_COEFF[i][0]*math.pow(temperature,-4)+PS_COEFF[i][1]*math.pow(temperature,-2)\
+        +PS_COEFF[i][2]*math.pow(temperature,-1)\
+        +PS_COEFF[i][3]+PS_COEFF[i][4]*temperature+PS_COEFF[i][5]*math.pow(temperature,2)
+
+
+
+#Calculating equation of state and fugacity
 
 #Solve Equation of state, Eq 2 of Pitzer and Sterner (1994)
 #Returns pressure in Pa
@@ -29,7 +70,7 @@ def PSfug(P,T,V):
     return math.exp(lnf)/1e6 # fugacity in MPa
 
 #Optimizing equation to solve for volume
-def fugacity_calculator(temperature,pressure):
+def fugacity_optimizer(temperature,pressure):
 
     def fun(v):
         return eos(temperature, v)- pressure
@@ -41,88 +82,90 @@ def fugacity_calculator(temperature,pressure):
     return fugacity
 
 
-PScoeff = np.zeros([10,10])
-
-PScoeff[0][2]=0.24657688*math.pow(10,6)
-PScoeff[0][3]=0.51359951*math.pow(10,2) 
-PScoeff[1][2]=0.58638965*math.pow(10,0) 
-PScoeff[1][3]=-0.28646939*math.pow(10,-2) 
-PScoeff[1][4]=0.31375577*math.pow(10,-4) 
-PScoeff[2][2]=-0.62783840*math.pow(10,1) 
-PScoeff[2][3]=0.14791599*math.pow(10,-1) 
-PScoeff[2][4]=0.35779579*math.pow(10,-3) 
-PScoeff[2][5]=0.15432925*math.pow(10,-7) 
-PScoeff[3][3]=-0.42719875*math.pow(10,0) 
-PScoeff[3][4]=-0.16325155*math.pow(10,-4) 
-PScoeff[4][2]=0.56654978*math.pow(10,4) 
-PScoeff[4][3]=-0.16580167*math.pow(10,2) 
-PScoeff[4][4]=0.76560762*math.pow(10,-1) 
-PScoeff[5][3]=0.10917883*math.pow(10,0) 
-PScoeff[6][0]=0.38878656*math.pow(10,13) 
-PScoeff[6][1]=-0.13494878*math.pow(10,9) 
-PScoeff[6][2]=0.30916564*math.pow(10,6)
-PScoeff[6][3]=0.75591105*math.pow(10,1) 
-PScoeff[7][2]=-0.65537898*math.pow(10,5) 
-PScoeff[7][3]=0.18810675*math.pow(10,3) 
-PScoeff[8][0]=-0.14182435*math.pow(10,14) 
-PScoeff[8][1]=0.18165390*math.pow(10,9) 
-PScoeff[8][2]=-0.19769068*math.pow(10,6)
-PScoeff[8][3]=-0.23530318*math.pow(10,2)
-PScoeff[9][2]=0.92093375*math.pow(10,5)
-PScoeff[9][3]=0.12246777*math.pow(10,3)
-
-cs = np.zeros([10]) 
-
-def calculate_coefficient_table(temperature):
-    for i in range(0, len(PScoeff)):
-        cs[i]=PScoeff[i][0]*math.pow(temperature,-4)+PScoeff[i][1]*math.pow(temperature,-2)\
-        +PScoeff[i][2]*math.pow(temperature,-1)\
-        +PScoeff[i][3]+PScoeff[i][4]*temperature+PScoeff[i][5]*math.pow(temperature,2)
 
 
 
-class FugacityCalculator():
-    def __init__(self, temperature_values, pressure_values):
-        if len(temperature_values) > 1:
-            self.temperature = C2K(np.arange(temperature_values[0],temperature_values[1]+1))
-        else:
-            self.temperature = C2K(temperature_values)
-        self.pressure = self.convert_mpa_to_pa(pressure_values)
-        self.temperature_and_pressure = self.combine_temp_and_pressure_values()
+
     
-    @staticmethod
-    def convert_mpa_to_pa(pressure_values):
-        if len(pressure_values) > 1:
-            pressures = np.arange(pressure_values[0],pressure_values[1]+1)
-            for i,p in enumerate(pressures):
-                Ppa = p*1.0E6
-                pressures[i] = Ppa
-        else:
-            pressures = np.array(pressure_values)*1.0E6
-        return pressures 
-    
-    def combine_temp_and_pressure_values(self):
-        all_values = []         
-        if len(self.temperature)>1:    
-            for i,temp in enumerate(self.temperature):
-                temp_and_pressure = (temp,self.pressure[i])
-                all_values.append(temp_and_pressure)
-        else:
-              all_values = (float(self.temperature), float(self.pressure))
-        return all_values
+
+
+#CALCULATE DIFFERENTIAL STRESS
+#takes paleopiezometer constants from the literature and input of grain size in microns
+
+#Constants
+CONSTANT_B = 2451 #(Holyoke 2010) #3631 (Stipp and Tullis, 2003)
+EXPONENT =  -1.26
+#1.45E4 -1.47 Twiss 1977 #Get more from literature
+
+
+#CALCULATE STRAIN RATE
+#Takes flow law constants from the literature and input of differential stress and fugacity, pressure and temperature
+
+FLOW_LAWS = {
+    "KT84": {"A": 2.2E-6, "n": 2.7, "Q": 1.2E5},
+    'GT95wm': {"A": 1.8E-8, "n": 4, "Q": 1.37E5},
+    'J84': {"A": 2.88E-3, "n": 1.8, "Q": 1.51E5},
+    'K89': {"A": 1.1E-6, "n": 2.7, "Q": 1.34E5},
+    'HC82': {"A": 1.99E-2, "n": 1.8, "Q": 1.67E5},
+    'LP92g': {"A": 6.6E-8, "n": 3.1, "Q": 1.35E5},
+    'LP92a': {"A": 3.98E-10, "n": 4, "Q": 1.35E5},
+    "H01":  {"A": 6.3E-12, "n": 4, "Q": 1.35E5}, 
+    "RB04": {"A": 1.2E-5, "n": 2.97, "Q": 2.42E5}
+    }
+
+
+    ##CALCULATE FUGACITY
+#Takes imputs of pressure and temperature converts them from MPa and C to Pa and K 
+
+class FlowStressCalculator():
+    def __init__(self, temperature_values, pressure_values, grain_size):
+        self.temperature = C2K(np.array(temperature_values))
+        self.pressure = np.array(pressure_values)*1.0E6
+        self.grain_size = grain_size
+        self.fugacity = []
+        self.differential_stress = []
+        self.strain_rate = []
+        self.slip_rate= []  
+
         
-    def calculate_fugacity(self): 
-        temperature_pressure_fugacity = []
-        if len(self.temperature)>1:
-            for t,p in self.temperature_and_pressure: 
+    def calculate_fugacity(self):
+        
+        for t in self.temperature:
+            for p in self.pressure:
                 calculate_coefficient_table(t)
-                fugacity = fugacity_calculator(t,p)
-                values = (t,p,fugacity)
-                temperature_pressure_fugacity.append(values)
-        else:
-            calculate_coefficient_table(self.temperature_and_pressure[0])
-            fugacity = fugacity_calculator(self.temperature_and_pressure[0], self.temperature_and_pressure[1])
-            temperature_pressure_fugacity = [(self.temperature_and_pressure[0], self.temperature_and_pressure[1], fugacity)]
+                fug = fugacity_optimizer(t,p)
+                self.fugacity.append(fug)
+        return self.fugacity
+
+
+    def calculate_differential_stress(self):
         
+        for grain in self.grain_size:
+            part = math.exp((math.log(grain)-math.log(CONSTANT_B))/EXPONENT)
+            self.differential_stress.append(part)
+        return self.differential_stress
+
+
+    def calculate_strain_rate(self, flow_law='H01'): 
+          
+        for t, f in zip(self.temperature, self.fugacity):
+            for stress in self.differential_stress:
+                sr = (FLOW_LAWS[flow_law]['A']*np.power(stress, FLOW_LAWS[flow_law]['n'])*np.power(f,1)*np.exp(-FLOW_LAWS[flow_law]['Q']/(8.3144598*t)))
+                self.strain_rate.append(sr)
+        
+        return self.strain_rate
+
+    def calculate_slip_rate(self, strain_rate, width): #width in m, output of mm/yr
+        
+        for w in width: 
+
+            for strain in strain_rate:
             
-        return temperature_pressure_fugacity
+                vel = w*1000*31536000*strain
+                self.slip_rate.append(vel)
+            
+        return self.slip_rate
+
+    
+
+
--- calculator/test_fugacity.py
+++ calculator/test_fugacity.py
@@ -1,12 +1,46 @@
+# import numpy as np
+# from scipy.constants.constants import C2K
 from fugacity import *
 import unittest 
 import pdb
 
-class Test_fugacity_calculator(unittest.TestCase):
+temperature = [450, 500]
+pressure = [400]
+grain_size =[10]
+width = [30]
+
+
+
+
+class Test_flow_stress_calculator(unittest.TestCase):
 
 	def setUp(self):
-		self.fugacity_calculation = FugacityCalculator(temperature, pressure)
+		self.fs_calculation = FlowStressCalculator(temperature, pressure, grain_size)
 
-	def test_if_fugacity_calculator_initializes(self):
-		self.assertEqual(self.fugacity_calculation[0][0], 723.15)
+	def test_if_flow_stress_calculator_initializes(self):
+		self.assertEqual(self.fs_calculation.temperature[1], 773.15)#723.15
+		self.assertEqual(self.fs_calculation.pressure, 400000000)
+		self.assertEqual(self.fs_calculation.grain_size, [10])
+		self.assertEqual(self.fs_calculation.fugacity,[])
+		self.assertEqual(self.fs_calculation.differential_stress,[])
+		self.assertEqual(self.fs_calculation.strain_rate,[])
 
+	def test_if_fugacity_calculator_works(self):
+		self.fs_calculation.calculate_fugacity()
+		self.assertEqual(self.fs_calculation.fugacity,[116.71651725233453, 149.72095732050246])
+
+	def test_if_differential_stress_is_calculated(self):
+		self.ds = self.fs_calculation.calculate_differential_stress() 
+		self.assertEqual(self.ds, [78.75972939002484])
+		
+	def test_if_strain_rate_is_calculated(self):
+
+		self.sr = self.fs_calculation.calculate_strain_rate(flow_law='H01')
+	
+		self.assertEqual(self.sr, [5.0181530333149198e-12])
+
+	def test_if_slip_rate_is_calculated(self):
+
+		self.slip = self.fs_calculation.calculate_slip_rate([5.01815303331492e-12], width)
+
+		self.assertEqual(self.slip, [4.7475742217585797])
